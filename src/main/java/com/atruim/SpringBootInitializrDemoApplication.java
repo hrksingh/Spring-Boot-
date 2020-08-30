3f7561780e3d0ed9d3928c6cd814a4946eaeebeb
Ashok <hrksingh17@gmail.com>
2020-08-30 12:52:33 +0530

    Spring Boot Demo stub

---
+++ src/main/java/com/atruim/SpringBootInitializrDemoApplication.java
@@ -0,0 +1,14 @@
+package com.atruim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitializrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitializrDemoApplication.class, args);
+		System.out.println("Demo Spring Project");
+	}
+
+}
---
+++ src/test/java/com/atruim/SpringBootInitializrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atruim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitializrDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
